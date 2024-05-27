ade5876c980753a2a32f9b7194bafe5971568e3b
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-27 10:42:07 +0530

    class based views

--- inkspire/inkspire_app/views.py
+++ inkspire/inkspire_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
@@ -8,6 +9,12 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    #<app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})   
