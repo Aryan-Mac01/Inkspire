b94a6e3cec474f331dbb72f5d93e6d0126d95c28
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-27 16:33:01 +0530

    each blog detail

--- inkspire/inkspire_app/views.py
+++ inkspire/inkspire_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -15,6 +15,13 @@ class PostListView(ListView):
     #<app>/<model>_<viewtype>.html
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    
+    
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})   
+    return render(request, 'blog/about.html', {'title': 'About'})
