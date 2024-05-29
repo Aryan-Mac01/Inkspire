5d71a28871a9258a87f62739280de48fbda0f8f9
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-05-29 22:29:14 +0530

    self post update

--- inkspire/inkspire_app/views.py
+++ inkspire/inkspire_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
@@ -31,7 +31,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title','content']
     template_name = 'blog/post_form.html'
@@ -39,7 +39,12 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
     
 
 def about(request):
