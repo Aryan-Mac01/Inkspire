8227937d0c9dc1eddaf0a174c67777b84e9ba1c6
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2024-06-02 14:48:10 +0530

    Total posts of a specific user

--- inkspire/inkspire_app/views.py
+++ inkspire/inkspire_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
@@ -18,6 +19,18 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    #<app>/<model>_<viewtype>.html
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
